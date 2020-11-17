7d8218d50c6add75a0c84a867555af7984492ac7
Amanda Hagy <amandahagy@hotmail.com>
2020-11-17 16:28:53 -0300

    ORM Hibernate

--- src/com/shiningstars/satellitelove/entity/User.java
+++ src/com/shiningstars/satellitelove/entity/User.java
@@ -3,9 +3,31 @@ package com.shiningstars.satellitelove.entity;
 import java.io.Serializable;
 import java.util.Calendar;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="T_SLS_USER")
+@SequenceGenerator(name="sqUser",
+sequenceName="SEQ_NAME", allocationSize=1)
 public class User implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="sqUser")
+	@Column(name = "cd_user")
 	private int id;
+	
 	private String email;
 	private String password;
 	private Calendar birthday;
